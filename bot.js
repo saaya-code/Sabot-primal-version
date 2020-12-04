9b1b58409835b52facbf63e1c29c414261c964e6
saàya <57566725+saaya-code@users.noreply.github.com>
2020-12-04 16:18:13 +0100

    changed to dotenv so i can publish it lol

---
+++ bot.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+client.login(process.env.TOKEN);
+client.on('ready', Readybot);
+function Readybot(){
+    console.log('I\'m ready!!');
+}
+//info
+client.on('message', info);
+function info(msg){    
+    if (msg.content.startsWith('/info')){
+
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const infoEmbed = new Discord.MessageEmbed()
+    .setColor(randomColor)
+    .setTitle('User info')
+    .setAuthor('saàya hh')
+    .addField('user Id : ', msg.author.id)
+    .addField('user name : ', msg.author.username)
+        msg.reply(infoEmbed)
+
+
+    }}
+    //reciever
+    client.on('message',logprv)
+    function logprv(msg){
+        if (msg.guild === null){
+            console.log('private message from : '+ msg.author.username+' content :  '+msg.content)
+        }
+    }
+
+    client.on('message', select)
+    function select(msg){
+        if (msg.content.startsWith('!select')){
+        str = msg.content.substring(8,msg.content.length)
+        if (str.includes('(')&&str.includes(')')){
+option = str.substring(str.indexOf('(')+1,str.indexOf(')'))
+str=str.substring(str.indexOf(')')+2,str.length)
+}
+else{
+    str = msg.content.substring(8,msg.content.length)
+ option ='Selection'
+}
+array = str.split(' ');
+random = Math.floor(Math.random()*array.length)
+
+console.log(option)
+console.log(array[random])
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+const Embedd = new Discord.MessageEmbed()
+             .setColor(randomColor)
+             .setTitle(option)
+             .addField("The bot choose",array[random])
+             msg.reply(Embedd)
+
+}
+    }
