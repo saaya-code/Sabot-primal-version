40a89437a23f10f83290b640624164d51d5cf4bf
saàya <57566725+saaya-code@users.noreply.github.com>
2021-01-27 08:51:03 +0100

    Update

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = async function (msg) {
     try{
     msg.channel.send("Loading data..").then(async msg1=>{
 
-        msg.reply(`Your client ping is ${msg1.createdTimestamp - msg.createdTimestamp}`)
+        msg.reply(`Bot's ping is ${msg1.createdTimestamp - msg.createdTimestamp} ms `)
         await msg1.delete()
 
     })
